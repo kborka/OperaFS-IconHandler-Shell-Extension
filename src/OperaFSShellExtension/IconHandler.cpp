c7aad7d5a2e53f9801ecf23406114de2abe37805
kborka <kborka@gmail.com>
2021-05-31 09:36:03 -0500

    Switched IconHandler from IPersistFile interface to IInitializeWithStream interface.

--- src/OperaFSShellExtension/IconHandler.cpp
+++ src/OperaFSShellExtension/IconHandler.cpp
@@ -14,7 +14,7 @@ extern HINSTANCE g_hInst;
 const CLSID CLSID_OperaFSShellExtension =
 { 0xdeadbeef, 0xdead, 0xbeef, { 0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef } };
 
-IconHandler::IconHandler() : m_cRef(1)
+IconHandler::IconHandler() : m_cRef(1), m_pStream(NULL)
 {
 	InterlockedIncrement(&g_cDllRef);
 }
@@ -29,7 +29,7 @@ IFACEMETHODIMP IconHandler::QueryInterface(REFIID riid, void **ppv)
 {
 	static const QITAB qit[] =
 	{
-		QITABENT(IconHandler, IPersistFile),
+		QITABENT(IconHandler, IInitializeWithStream),
 		QITABENT(IconHandler, IExtractIconW),
 		{ 0 },
 	};
@@ -55,24 +55,16 @@ IFACEMETHODIMP_(ULONG) IconHandler::Release()
 }
 
 // IInitializeWithStream Implementation
-// TODO: Figure this out.
-//IFACEMETHODIMP IconHandler::Initialize(IStream *pstream, DWORD grfMode)
-//{
-//	HRESULT hr = HRESULT_FROM_WIN32(ERROR_ALREADY_INITIALIZED);
-//
-//	if (m_pStream == NULL)
-//	{
-//		hr = pstream->QueryInterface(&m_pStream);
-//	}
-//
-//	return hr;
-//}
-
-// IPersistFile Implementation
-IFACEMETHODIMP IconHandler::Load(LPCOLESTR pszFileName, DWORD dwMode)
+IFACEMETHODIMP IconHandler::Initialize(IStream *pstream, DWORD grfMode)
 {
-	m_dwMode = dwMode;
-	return StringCchCopy(m_szFileName, ARRAYSIZE(m_szFileName), pszFileName);
+	HRESULT hr = HRESULT_FROM_WIN32(ERROR_ALREADY_INITIALIZED);
+
+	if (m_pStream == NULL)
+	{
+		hr = pstream->QueryInterface(&m_pStream);
+	}
+
+	return hr;
 }
 
 // IExtractIcon Implementation
@@ -83,26 +75,11 @@ IFACEMETHODIMP IconHandler::Extract(PCWSTR pszFile, UINT nIconIndex, HICON *phic
 
 IFACEMETHODIMP IconHandler::GetIconLocation(UINT uFlags, PWSTR pszIconFile, UINT cchMax, int *piIndex, UINT *pwFlags)
 {
-	HANDLE hFile;
-	hFile = CreateFile(m_szFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-	if (hFile == INVALID_HANDLE_VALUE)
-	{
-		return S_FALSE;
-	}
-
-	
-
 	char duck[] = { 'd', 'u', 'c', 'k' };
 	char duckBytes[4];
 
-	// Move to 0x84.
-	OVERLAPPED ol{};
-	ol.Offset = 132;
-	ReadFile(hFile, duckBytes, ARRAYSIZE(duckBytes), NULL, &ol);
-
-	//m_pStream->Seek({ 132 }, STREAM_SEEK_SET, NULL);
-	//m_pStream->Read(duckBytes, ARRAYSIZE(duckBytes), NULL);
+	m_pStream->Seek({ 132 }, STREAM_SEEK_SET, NULL);
+	m_pStream->Read(duckBytes, ARRAYSIZE(duckBytes), NULL);
 
 	for (int i = 0; i < ARRAYSIZE(duck); i++)
 	{
@@ -111,7 +88,6 @@ IFACEMETHODIMP IconHandler::GetIconLocation(UINT uFlags, PWSTR pszIconFile, UINT
 			return S_FALSE;
 		}
 	}
-
 	
 	GetModuleFileName(g_hInst, pszIconFile, cchMax);		
 
--- src/OperaFSShellExtension/IconHandler.h
+++ src/OperaFSShellExtension/IconHandler.h
@@ -3,7 +3,7 @@
 #include <ShlObj.h>
 
 class IconHandler : 
-	public IPersistFile, 
+	public IInitializeWithStream,
 	public IExtractIconW
 {
 public:
@@ -12,16 +12,8 @@ public:
 	IFACEMETHODIMP_(ULONG) AddRef();
 	IFACEMETHODIMP_(ULONG) Release();
 
-	// IPersistFile implemenation
-	IFACEMETHODIMP GetClassID(CLSID *pclsid) { return E_NOTIMPL; };
-	IFACEMETHODIMP GetCurFile(LPOLESTR *ppszFileName) { return E_NOTIMPL; };
-	IFACEMETHODIMP IsDirty() { return E_NOTIMPL; };
-	IFACEMETHODIMP Load(LPCOLESTR pszFileName, DWORD dwMode);
-	IFACEMETHODIMP Save(LPCOLESTR pszFileName, BOOL fRemember) { return E_NOTIMPL; };
-	IFACEMETHODIMP SaveCompleted(LPCOLESTR pszFileName) { return E_NOTIMPL; };
-
 	// IInitializeWithStream Implementation
-	//IFACEMETHODIMP Initialize(IStream *pstream, DWORD grfMode);
+	IFACEMETHODIMP Initialize(IStream *pstream, DWORD grfMode);
 
 	// IExtractIcon implemenatation
 	IFACEMETHODIMP Extract(PCWSTR pszFile, UINT nIconIndex, HICON *phiconLarge, HICON *phiconSmall, UINT nIconSize);
@@ -34,8 +26,6 @@ protected:
 
 private:
 	long m_cRef;
-	wchar_t m_szFileName[MAX_PATH];
-	DWORD m_dwMode;
-	//IStream *m_pStream;
+	IStream *m_pStream;
 };
 
